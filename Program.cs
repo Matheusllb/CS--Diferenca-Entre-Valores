145baccff6987defdad6b593f322a69d2a908400
Matheusllb <matheusluislopesbatista@gmail.com>
2023-03-31 14:48:03 -0300

    Terceiro exercício em CSharp

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CsE3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCICIO 3
+            Console.WriteLine("Calculando a diferenca entre valores inteiros\n\nValor A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor C: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor D: ");
+            int d = int.Parse(Console.ReadLine());
+
+            int dif = (a * b - c * d);
+
+            Console.WriteLine("\nDiferença: " + dif);
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
